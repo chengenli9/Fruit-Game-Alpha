5fccf1ddf7e9186b12a9dc508e51b50db900012a
Chengen Li <62069938+chengenli9@users.noreply.github.com>
2024-12-24 00:19:39 -0800

    testing

--- Player.js
+++ Player.js
@@ -8,7 +8,7 @@ export default class Player {
     this.color = color;
 
     this.playerImage = new Image();
-    this.playerImage.src = 'images/face2.png';
+    this.playerImage.src = 'images/smile_processed.webp';
     this.image = this.playerImage;
   }
 
